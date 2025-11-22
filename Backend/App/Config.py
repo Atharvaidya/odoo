bc8f700aedc693df4e9da98c1d747f50270be72b
Parth Surve <parthamolsurve@gmail.com>
2025-11-22 11:25:21 +0530

    mainpython

---
+++ Backend/App/Config.py
@@ -0,0 +1,10 @@
+# backend/app/config.py
+import os
+
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASS = os.getenv("DB_PASS", "")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_NAME = os.getenv("DB_NAME", "stockmaster")
+
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}?charset=utf8mb4"
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
